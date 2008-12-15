44b1f878ebf2852d6f755cf61760a5674acac77c
salluu <salluu@b765f2fe-774f-0410-9567-b5325461a675>
2008-12-15 08:58:20 +0000

    added  comments to functions and re-factor the code

--- WebFoundations/controls/LanguageSelector.ascx.cs
+++ WebFoundations/controls/LanguageSelector.ascx.cs
@@ -10,35 +10,40 @@ public partial class controls_LanguageSelector : System.Web.UI.UserControl
     {
         if (!IsPostBack)
         {
+            // binding dropdownlist for language selector
             BindDDLLanguage();
             // keep selected language on !postback 
             if (Session["Language"] != null)
-              ddlLanguage.SelectedValue = Convert.ToString(Session["Language"]);
+                ddlLanguage.SelectedValue = Convert.ToString(Session["Language"]);
         }
 
+        // using query string to select default language
         if (Request.QueryString["lang"] != null)
         {
             BindDDLLanguage();
             ddlLanguage.SelectedValue = Convert.ToString(Request.QueryString["lang"]);
             Session["Language"] = ddlLanguage.SelectedValue;
+            // redirect is necessary in order to  run InitializeCulture in BasePage.cs
             Response.Redirect(Context.Request.Path);
 
         }
         if (Session["Language"] == null)
         {
+            // when loading first time get user language  preference
             string userLanguage = CMS.LanguageCodeGet(System.Web.HttpContext.Current.Request.UserLanguages[0]);
-           bool exists=CMS.LanguageExits(userLanguage);
-           if (!exists)
-               ddlLanguage.SelectedValue = System.Configuration.ConfigurationManager.AppSettings["DefaultLanguage"].ToString();
-           else
-               ddlLanguage.SelectedValue = userLanguage;
+            // if user prefer language doesnot exist select the default language
+            bool exists = CMS.LanguageExits(userLanguage);
+            if (!exists)
+                ddlLanguage.SelectedValue = System.Configuration.ConfigurationManager.AppSettings["DefaultLanguage"].ToString();
+            else
+                ddlLanguage.SelectedValue = userLanguage;
 
-           Session["Language"] = ddlLanguage.SelectedValue;
+            Session["Language"] = ddlLanguage.SelectedValue;
         }
 
-        
-    }
 
+    }
+    
     protected void BindDDLLanguage()
     {
         XmlDataSource xmlLanguages = new XmlDataSource();
